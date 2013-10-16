5279b1741ecd200acb300acdbdc4cb4e9aa2ab8a
liuyi <liuyi@github.com>
2013-10-16 13:43:54 +0800

    updated android class

--- src/android/JsUtils.java
+++ src/android/JsUtils.java
@@ -1,10 +1,11 @@
-package com.dooya.plugin;
+package com.ourbrander.pgPlugin;
 
 import java.io.UnsupportedEncodingException;
 
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
  
+ 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
  
